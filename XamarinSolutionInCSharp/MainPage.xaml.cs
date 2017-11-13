086662ac80dfd2bd2cd19fd88f6b6296b0bf1c13
Richard Hill <richardhill101@gmail.com>
2017-11-13 07:59:45 +0000

    Check-in

---
+++ XamarinSolutionInCSharp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinSolutionInCSharp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Content = CreatePage();
+            InitializeComponent();
+        }
+        private View CreatePage()
+        {
+            var outerStackLayout = new StackLayout()
+            {
+                Padding=30,
+                Children = {
+                    new Label(){}
+            };
+        }
+    }
+}
