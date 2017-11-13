63bfe4391ab7bceab89f217182a9aea0fbcde806
Richard Hill <richardhill101@gmail.com>
2017-11-13 07:54:52 +0000

    First Commit

---
+++ XamarinSolutionInCSharp/XamarinSolutionInCSharpPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinSolutionInCSharp
+{
+    public partial class XamarinSolutionInCSharpPage : ContentPage
+    {
+        public XamarinSolutionInCSharpPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
